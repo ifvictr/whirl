6ed79815350010a7085eb2be8e2b23fe9aaba28f
Victor Truong <victor@victortruong.com>
2020-06-17 01:10:17 -0700

    Use parseInt on port config value

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 interface WhirlConfig {
     databaseUrl?: string
-    port: number | string
+    port: number
     botToken?: string
     signingSecret?: string
 }
 
 const config: WhirlConfig = {
     databaseUrl: process.env.DATABASE_URL || process.env.MONGODB_URI,
-    port: process.env.PORT || 3000,
+    port: parseInt(process.env.PORT || '') || 3000,
     // Slack-specific config
     botToken: process.env.SLACK_CLIENT_BOT_TOKEN,
     signingSecret: process.env.SLACK_CLIENT_SIGNING_SECRET

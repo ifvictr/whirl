5c7dff4a957dcb61c5c9fb215048d7a8b22e7c8e
Victor Truong <victor@victortruong.com>
2020-06-29 15:33:55 -0700

    Switch config key order

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 interface WhirlConfig {
-    redisUrl: string
     port: number
+    redisUrl: string
     botToken?: string
     signingSecret?: string
 }
 
 const config: WhirlConfig = {
-    redisUrl: process.env.REDIS_URL || '',
     port: parseInt(process.env.PORT || '') || 3000,
+    redisUrl: process.env.REDIS_URL || '',
     // Slack-specific config
     botToken: process.env.SLACK_CLIENT_BOT_TOKEN,
     signingSecret: process.env.SLACK_CLIENT_SIGNING_SECRET

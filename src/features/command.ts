24ef37c29689c2aa54f20e44db8abb028593001f
Victor Truong <victor@victortruong.com>
2020-07-14 01:45:59 -0700

    Auto-add the user to the pool on /next
    This code was copied from chat_start. I need to abstract the logic out but I'm too lazy right now 😛

--- src/features/command.ts
+++ src/features/command.ts
@@ -1,6 +1,7 @@
 import { App } from '@slack/bolt'
 import { ChatPrompt } from '../blocks'
 import { Chat, User } from '../models'
+import pool from '../pool'
 import { capitalize, getEmoji } from '../utils'
 
 export default (app: App) => {
@@ -13,54 +14,85 @@ export default (app: App) => {
             return
         }
 
-        if (!await user.isInChat()) {
-            await respond('You’re not in a chat.')
+        // User can't join the pool again if they're in it
+        if (await user.isInPool()) {
+            await client.chat.postEphemeral({
+                channel: command.channel_id,
+                user: command.user_id,
+                text: 'You’re already waiting in the pool.'
+            })
             return
         }
 
-        const chat = await user.getCurrentChat() as Chat
+        if (await user.isInChat()) {
+            const chat = await user.getCurrentChat() as Chat
 
-        // Broadcast leave message to current chat
-        const noun = await user.getNoun() as string
-        const displayName = `Anonymous ${capitalize(noun)}`
-        const emoji = getEmoji(noun) as string
-        const message = `:${emoji}: _${displayName} has left the chat._`
-        for (const memberId of await chat.getMembers()) {
-            if (memberId === command.user_id) {
-                continue
+            // Broadcast leave message to current chat
+            const noun = await user.getNoun() as string
+            const displayName = `Anonymous ${capitalize(noun)}`
+            const emoji = getEmoji(noun) as string
+            const message = `:${emoji}: _${displayName} has left the chat._`
+            for (const memberId of await chat.getMembers()) {
+                if (memberId === command.user_id) {
+                    continue
+                }
+
+                await client.chat.postMessage({
+                    channel: memberId,
+                    text: message
+                })
             }
 
-            await client.chat.postMessage({
-                channel: memberId,
-                text: message
-            })
+            await user.leave()
+
+            // Show messsage and prompt to leaving user
+            await say('_You’ve left the chat._')
+
+            // Kick remaining members if the chat size drops below 2
+            const updatedMembers = await chat.getMembers()
+            if (updatedMembers.length < 2) {
+                for (const memberId of updatedMembers) {
+                    await client.chat.postMessage({
+                        channel: memberId,
+                        text: '_This chat has ended._'
+                    })
+                    await client.chat.postMessage({
+                        channel: memberId,
+                        text: 'Want to start another chat?',
+                        blocks: ChatPrompt()
+                    })
+                }
+                await chat.end()
+            }
         }
 
-        await user.leave()
+        // Attempt to create a chat. If that fails, add the user to the pool.
+        const newChat = await pool.createChat(command.user_id)
+        if (!newChat) {
+            await pool.add(command.user_id)
+            await say('You’ve been added to *The Waiting Pool* :beach_with_umbrella:! A chat will start as soon as more people join.')
+            return
+        }
 
-        // Show messsage and prompt to leaving user
-        await say('_You’ve left the chat._')
-        await client.chat.postMessage({
-            channel: command.user_id,
-            text: 'Want to start another chat?',
-            blocks: ChatPrompt()
-        })
+        // Introduce members to each other
+        const members = await newChat.getMembers()
+        // Loop over all the identities of the chat members
+        for (const memberId of members) {
+            const member = await User.get(memberId) as User
 
-        // Kick remaining members if the chat size drops below 2
-        const updatedMembers = await chat.getMembers()
-        if (updatedMembers.length < 2) {
-            for (const memberId of updatedMembers) {
+            const noun = await member.getNoun() as string
+            const displayName = `Anonymous ${capitalize(noun)}`
+            const emoji = getEmoji(noun) as string
+            // Send intro message to everyone but the member being introduced
+            for (const otherMemberId of members) {
+                if (otherMemberId === memberId) {
+                    continue
+                }
                 await client.chat.postMessage({
-                    channel: memberId,
-                    text: '_This chat has ended._'
-                })
-                await client.chat.postMessage({
-                    channel: memberId,
-                    text: 'Want to start another chat?',
-                    blocks: ChatPrompt()
+                    channel: otherMemberId,
+                    text: `You are now talking to :${emoji}: *${displayName}*. Say hi!`
                 })
             }
-            await chat.end()
         }
     })
 }

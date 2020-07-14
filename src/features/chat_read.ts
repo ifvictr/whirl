017c768cb821534350197c84af5b941f2be41abb
Victor Truong <victor@victortruong.com>
2020-07-13 20:19:08 -0700

    Fix latest read message ID not being set because of rejection

--- src/features/chat_read.ts
+++ src/features/chat_read.ts
@@ -32,18 +32,17 @@ export default (app: App) => {
 
             const member = await User.get(memberId) as User
             const dmChannelId = await member.getDmChannelId() as string
+            const lastReadMessageId = await user.getLastReadMessageId(memberId) as string
+            const { messages } = await client.conversations.history({
+                channel: dmChannelId,
+                limit: 1
+            }) as any
+            const latestMessageId = messages[0].ts
             try {
-                const lastReadMessageId = await user.getLastReadMessageId(memberId) as string
-                const { messages } = await client.conversations.history({
-                    channel: dmChannelId,
-                    limit: 1
-                }) as any
-
                 // Check the ID of the latest message. If it matches the previously saved
                 // ID, then we know that there haven't been any new messages sent in
                 // the chat. This also prevents unnecessary API calls from being
                 // made by the read receipt getting removed and immediately re-added.
-                const latestMessageId = messages[0].ts
                 if (lastReadMessageId === latestMessageId) {
                     break
                 }
@@ -63,12 +62,12 @@ export default (app: App) => {
                     name: emoji,
                     timestamp: latestMessageId
                 })
-
+            } catch (e) {
+                // Ignore rejections like having already reacted, message not existing, etc.
+            } finally {
                 // Save the ID of the latest message so we can remove the reaction
                 // from it if the receipt needs to be updated in the future.
                 await user.setLastReadMessageId(memberId, latestMessageId)
-            } catch (e) {
-                console.log(e)
             }
         }
     })

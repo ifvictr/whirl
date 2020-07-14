d8beb43d0039289e42f8b1ce28c09bcd193ee7fd
Victor Truong <victor@victortruong.com>
2020-07-13 20:09:22 -0700

    Write better comments

--- src/features/chat_read.ts
+++ src/features/chat_read.ts
@@ -13,19 +13,19 @@ export default (app: App) => {
             return
         }
 
-        // Read receipts are only shown to a user if they're in a chat
+        // Only show read receipts to users that are in a chat
         if (!await user.isInChat()) {
             return
         }
 
         const chat = await user.getCurrentChat() as Chat
 
-        // Update read receipt for all members of the chat
+        // Send a read receipt to everyone else in the chat
         const members = await chat.getMembers()
         const noun = await user.getNoun() as string
         const emoji = members.length > 2 ? getEmoji(noun) as string : 'eyes'
         for (const memberId of members) {
-            // Don't show a read receipt to the viewing user
+            // Don't send a receipt to self
             if (memberId === event.user) {
                 continue
             }
@@ -39,15 +39,16 @@ export default (app: App) => {
                     limit: 1
                 }) as any
 
-                // Check the timestamp of the last message. If it matches the
-                // previously saved ID, we know that there haven't been any new messages
-                // sent in the chat and immediately stop sending read receipts to anyone.
-                const latestReadMessageId = messages[0].ts
-                if (lastReadMessageId === latestReadMessageId) {
+                // Check the ID of the latest message. If it matches the previously saved
+                // ID, then we know that there haven't been any new messages sent in
+                // the chat. This also prevents unnecessary API calls from being
+                // made by the read receipt getting removed and immediately re-added.
+                const latestMessageId = messages[0].ts
+                if (lastReadMessageId === latestMessageId) {
                     break
                 }
 
-                // Remove the eyes reaction from the old latest message if it exists
+                // Remove the reaction from the previous latest message if it exists
                 if (lastReadMessageId) {
                     await client.reactions.remove({
                         channel: dmChannelId,
@@ -56,15 +57,16 @@ export default (app: App) => {
                     })
                 }
 
-                // Add the eyes reaction to the new latest message
+                // React to the latest message
                 await client.reactions.add({
                     channel: dmChannelId,
                     name: emoji,
-                    timestamp: latestReadMessageId
+                    timestamp: latestMessageId
                 })
 
-                // Save the ID of the latest message somewhere so we can remove the react later
-                await user.setLastReadMessageId(memberId, latestReadMessageId)
+                // Save the ID of the latest message so we can remove the reaction
+                // from it if the receipt needs to be updated in the future.
+                await user.setLastReadMessageId(memberId, latestMessageId)
             } catch (e) {
                 console.log(e)
             }

3f8216c230dc1e879748cfeceb8c2d6d73e439fc
Victor Truong <victor@victortruong.com>
2020-07-14 02:03:48 -0700

    Remove debug code

--- src/features/react.ts
+++ src/features/react.ts
@@ -2,7 +2,6 @@ import { App } from '@slack/bolt'
 
 export default (app: App) => {
     app.event('reaction_added', async ({ client, event }) => {
-        console.log(event)
         await client.chat.postEphemeral({
             // @ts-ignore
             channel: event.item.channel,
@@ -10,8 +9,4 @@ export default (app: App) => {
             text: 'Reactions aren’t currently supported, so only you are able to see them.'
         })
     })
-
-    // app.event('reaction_removed', async ({ event }) => {
-    //     console.log(event)
-    // })
 }

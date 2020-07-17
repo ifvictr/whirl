90822b10871d842be206404034e99d247cf569de
Victor Truong <victor@victortruong.com>
2020-07-17 05:31:34 -0700

    Only act on reaction_added events in DMs and if the user is in a chat

--- src/features/react.ts
+++ src/features/react.ts
@@ -1,7 +1,24 @@
 import { App } from '@slack/bolt'
+import { User } from '../models'
 
 export default (app: App) => {
     app.event('reaction_added', async ({ client, event }) => {
+        // @ts-ignore
+        const isDM = event.item.channel.startsWith('D')
+        if (!isDM) {
+            return
+        }
+
+        const user = await User.get(event.user)
+        if (!user) {
+            return
+        }
+
+        // We don't care if a user reacts and they're not in a chat
+        if (!await user.isInChat()) {
+            return
+        }
+
         await client.chat.postEphemeral({
             // @ts-ignore
             channel: event.item.channel,

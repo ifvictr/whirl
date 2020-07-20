2a22f4a114765736b6ef3cdd03918fc06bcca7b1
Victor Truong <victor@victortruong.com>
2020-07-20 05:55:04 -0700

    Restructure read receipt error catching

--- src/features/chat_read.ts
+++ src/features/chat_read.ts
@@ -32,44 +32,49 @@ export default (app: App) => {
 
             const member = await User.get(memberId) as User
             const dmChannelId = await member.getDmChannelId() as string
-            const lastReadMessageId = await user.getLastReadMessageId(memberId) as string
+            const lastReadMessageId = await user.getLastReadMessageId(memberId)
             const { messages } = await client.conversations.history({
                 channel: dmChannelId,
                 limit: 1
             }) as any
             const latestMessageId = messages[0].ts
-            try {
-                // Check the ID of the latest message. If it matches the previously saved
-                // ID, then we know that there haven't been any new messages sent in
-                // the chat. This also prevents unnecessary API calls from being
-                // made by the read receipt getting removed and immediately re-added.
-                if (lastReadMessageId === latestMessageId) {
-                    break
-                }
 
-                // Remove the reaction from the previous latest message if it exists
-                const reactOptions = {
-                    channel: dmChannelId,
-                    name: emoji
-                }
-                if (lastReadMessageId) {
+            // Check the ID of the latest message. If it matches the previously saved
+            // ID, then we know that there haven't been any new messages sent in
+            // the chat. This also prevents unnecessary API calls from being
+            // made by the read receipt getting removed and immediately re-added.
+            if (lastReadMessageId === latestMessageId) {
+                break
+            }
+
+            // Remove the reaction from the previous latest message, if it exists.
+            const reactOptions = {
+                channel: dmChannelId,
+                name: emoji
+            }
+            if (lastReadMessageId !== null) {
+                try {
                     await client.reactions.remove({
                         ...reactOptions,
                         timestamp: lastReadMessageId
                     })
+                } catch {
+                    // Ignore errors like `message_not_found` and `no_reaction`
+                    // because any non-existent messages associated with a read
+                    // receipt will be overwritten when the last read message ID
+                    // is set. Would only happen if a user sent a message, then
+                    // deleted it after everyone else has received it.
                 }
-                // React to the latest message
-                await client.reactions.add({
-                    ...reactOptions,
-                    timestamp: latestMessageId
-                })
-            } catch (e) {
-                // Ignore rejections like having already reacted, message not existing, etc.
-            } finally {
-                // Save the ID of the latest message so we can remove the reaction
-                // from it if the receipt needs to be updated in the future.
-                await user.setLastReadMessageId(memberId, latestMessageId)
             }
+
+            // Save the ID of the latest message so we can remove the reaction
+            // from it if the receipt needs to be updated in the future. Then apply
+            // the reaction to that message.
+            await user.setLastReadMessageId(memberId, latestMessageId)
+            await client.reactions.add({
+                ...reactOptions,
+                timestamp: latestMessageId
+            })
         }
     })
 }

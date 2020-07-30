2c51aa2b14640617a10df7a7c84304dd68eb7d47
Victor Truong <victor@victortruong.com>
2020-07-29 19:53:11 -0700

    Use headers in home tab

--- src/blocks/home_layout.ts
+++ src/blocks/home_layout.ts
@@ -28,20 +28,34 @@ export const HomeLayout = ({
             type: 'divider'
         },
         {
+            type: 'header',
+            text: {
+                type: 'plain_text',
+                text: 'Live Stats'
+            }
+        } as any,
+        {
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `*Live Stats* :bar_chart:\n
+                text: `
 :man-surfing: *${activeUserCount}* ${activeUserCount === 1 ? 'person is' : 'people are'} chatting right now${activeChatCount > 1 ? ` in *${activeChatCount}* different chats` : ''}\n
 :busts_in_silhouette: *${totalChatCount}* chat${totalChatCount === 1 ? ' has' : 's have'} been started with Whirl\n
 :speech_balloon: *${totalMessagesSent}* message${totalMessagesSent === 1 ? ' has' : 's have'} been sent in total by *${totalUserCount}* ${totalUserCount === 1 ? 'person' : 'people'}`
             }
         },
         {
+            type: 'header',
+            text: {
+                type: 'plain_text',
+                text: 'Quick Info'
+            }
+        } as any,
+        {
             type: 'section',
             text: {
                 type: 'mrkdwn',
-                text: `*Quick Info* :information_source:\n
+                text: `
 • A read receipt in the form of an :eyes: reaction (or animal emoji like :monkey_face:, depending on the group size) will appear under a message when another user reads it.
 • *\`/next\`:* Go to the next chat. Will add you to the waiting pool if there are no chats that can be immediately joined.
 • *\`/end\`:* Ends the current chat.`

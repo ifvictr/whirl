2dcd118fd125420294bbd9b1033be197db5abde9
Victor Truong <victor@victortruong.com>
2020-08-02 05:05:48 -0700

    Add user to DB when they're in app home

--- src/features/onboard.ts
+++ src/features/onboard.ts
@@ -4,10 +4,6 @@ import { User } from '../models'
 
 export default (app: App) => {
   app.event('app_home_opened', async ({ event, say }) => {
-    if (event.tab !== 'messages') {
-      return
-    }
-
     // Show intro message if this is the user's first time in a DM with Whirl
     if (!(await User.exists(event.user))) {
       await User.create(event.user, event.channel)

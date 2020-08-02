45edcf5fac575efcacb9cb63cffef08213a9c1c6
Victor Truong <victor@victortruong.com>
2020-08-02 05:07:04 -0700

    Use early return in onboard handler

--- src/features/onboard.ts
+++ src/features/onboard.ts
@@ -5,13 +5,15 @@ import { User } from '../models'
 export default (app: App) => {
   app.event('app_home_opened', async ({ event, say }) => {
     // Show intro message if this is the user's first time in a DM with Whirl
-    if (!(await User.exists(event.user))) {
-      await User.create(event.user, event.channel)
-      await say({
-        blocks: ChatPrompt({ isExistingUser: false }),
-        text:
-          'Welcome to Whirl, where you can have fun, completely anonymous chats with other members of your Slack. Check your DMs to get started.'
-      })
+    if (await User.exists(event.user)) {
+      return
     }
+
+    await User.create(event.user, event.channel)
+    await say({
+      blocks: ChatPrompt({ isExistingUser: false }),
+      text:
+        'Welcome to Whirl, where you can have fun, completely anonymous chats with other members of your Slack. Check your DMs to get started.'
+    })
   })
 }
